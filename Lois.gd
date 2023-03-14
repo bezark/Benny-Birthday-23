6d7806d7da3ba42e1a772bfcade4fec5722ca7b5
bezark <johnbezark@gmail.com>
2023-03-14 14:56:20 -0400

    Intro Scene Env

---
+++ Lois.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+
+
+	# Handle Jump.
+
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var xdirection = Input.get_axis("ui_left", "ui_right")
+	if xdirection:
+		velocity.x = xdirection * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var ydirection = Input.get_axis("ui_up", "ui_down")
+	if ydirection:
+		velocity.y = ydirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
